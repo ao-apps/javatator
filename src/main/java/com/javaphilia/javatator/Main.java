a28ca868937b429923d02c4e791b576a62ca9a43
AO Industries, Inc. <support@aoindustries.com>
2019-07-06 04:26:58 -0500

    Updated GPLv2 header comments to current standards.

--- src/main/java/com/javaphilia/javatator/Main.java
+++ src/main/java/com/javaphilia/javatator/Main.java
@@ -1,28 +1,30 @@
-package com.javaphilia.javatator;
-
-/**
- * Javatator - multi-database admin tool
- * 
+/*
+ * javatator - Multi-database admin tool.
+ *
  * Copyright (C) 2001  Jason Davies.
- * 
+ *     If you want to help or want to report any bugs, please email me:
+ *     jason@javaphilia.com
+ *
+ * Copyright (C) 2018, 2019  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
- * If you want to help or want to report any bugs, please email me:
- * jason@javaphilia.com
- * 
  */
+package com.javaphilia.javatator;
 
 import java.io.IOException;
 import java.util.List;
